2854ed8f3786f20e15b73a0a955e0bd4abeace55
Jon Mockbee <mockbee1000@gmail.com>
2019-07-08 16:19:26 -0400

    initial commit

---
+++ src/speedconverter.java
@@ -0,0 +1,13 @@
+public class speedconverter {
+
+    public static long  toMilesPerHour(double kilometersPerHour){
+
+        if (kilometersPerHour <0){
+            return -1
+        }
+
+       return = Math.round(kilometersPerHour/1.609);
+        
+    }
+
+}
