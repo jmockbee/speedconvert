0e38fa3395c9cd819acefe6b9285ab79892c6daf
Jon Mockbee <mockbee1000@gmail.com>
2019-07-09 16:31:28 -0400

    this did not turn out last time works now

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+       long miles = speedconverter.toMilesPerHour(10.5);
+        System.out.println("Miles = " + miles);
+
+        speedconverter.printConversion(10.5);
+    }
 }
--- src/speedconverter.java
+++ src/speedconverter.java
@@ -3,11 +3,21 @@ public class speedconverter {
     public static long  toMilesPerHour(double kilometersPerHour){
 
         if (kilometersPerHour <0){
-            return -1
+            return -1;
         }
 
-       return = Math.round(kilometersPerHour/1.609);
-        
-    }
+       return Math.round(kilometersPerHour/1.609);
 
+    }
+    public static void printConversion(double kilometersPerHour) {
+
+        if (kilometersPerHour <0) {
+            System.out.println( "invalid value");
+            } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour+" km/h = " + milesPerHour +
+            " mi/h");
+        }
+
+    }
 }
